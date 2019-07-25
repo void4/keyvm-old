0362b216386a973f610c08b181c524813c29a08a
Noreply <noreply@github.com>
2019-07-25 20:49:51 +0200

    Try fuzzing

--- main.py
+++ main.py
@@ -98,9 +98,165 @@ def pretty(p):
 
 # does it always jump back to the minimum of all header resources?
 
-#def decode():
-#def start()
+def run(code):
+	#TODO: make CAPS dict? other data structure?
+	process = [[S_NORMAL,0,1000,1000,0], {0}, code, []]
+	world = [process]
 
+	def debug():
+		print("\nSTEP#"	 + str(STEP), INAMES[I])
+		for proc in world:
+			print(pretty(proc))
+
+	start = 0
+	chain = [start]
+
+	while True:
+		current = chain[-1]
+		rec = world[current][HEADER][H_REC]
+		if rec != current:
+			chain.append(rec)
+		else:
+			break
+
+	print("CHAIN", chain)
+	#traverse H_REC here?
+
+	STEP = 0
+	while True:
+		STEP += 1
+
+		# TODO add jump-back logic to last process in parent chain that has sufficient resources
+
+		if len(chain) == 0:
+			print("END")
+			return world
+			#break
+		this = world[chain[-1]]
+
+		#TODO: only deserialize here, on demand
+
+		header = this[HEADER]
+		caps = this[CAPS]
+		code = this[CODE]
+
+		JB = False
+
+		def jump_back(condition, to=None):
+			nonlocal chain, JB
+			JB = True
+			header[H_STATUS] = condition
+			# TODO make this nicer
+			if to is None:
+				#world[active] = flat(world[active])
+				chain = chain[:-1]
+			else:
+				for i in range(len(world)-1, to, -1):
+					world[i] = flat(world[i])
+					chain = chain[:-1]
+
+		def set_cap(target):
+			caps.add(target)
+
+		def has_cap(target):
+			return target in caps
+
+		def transfer_cap(index, table):
+			if index in caps:
+				table.add(index)
+
+		ip = header[H_IP]
+		if ip >= len(code):#could also wrap around for shits and giggles
+			jump_back(S_OOC)
+			continue
+
+		#print(ip, len(code))
+		C = code[ip]
+		I = C[0]
+		#TODO check length
+		args = C[1:]
+
+		gascost = IGASCOSTS[I]
+		if gascost is None:
+			gascost = args[1]#size arg for ALLOC
+
+		memcost = IMEMCOSTS[I]
+		if memcost is None:
+			if I == I_ALLOC:
+				memcost = args[1]#size arg for ALLOC
+			elif I == I_TRANSFERCAP:
+				memcost = sum([1 for c in args[1:] if c is not None])
+
+		for node_index, node in enumerate(chain):
+			node_header = world[node][HEADER]
+			if node_header[H_GAS] < gascost:
+				jump_back(S_OOG, node_index-1)
+				break
+			if node_header[H_MEM] < memcost:
+				jump_back(S_OOM, node_index-1)
+				break
+
+		if JB:
+			continue
+
+		# First check all conditions, then change the state!
+		for node_index, node in enumerate(chain):
+			world[node][HEADER][H_GAS] -= gascost
+			world[node][HEADER][H_MEM] -= memcost
+
+		#debug()
+
+		header[H_IP] += 1
+
+		if I == I_CREATE:
+			memory = args[0]
+			index = len(world)
+
+			newproc = sharp(this[DATA][memory])
+			newproc[HEADER] = [S_NORMAL, 0, 0, 0, 0]
+			newproc[CAPS] = set()
+			#print("NEW", newproc)
+			world.append(newproc)
+
+			set_cap(index)
+
+		elif I == I_ALLOC:
+			memory, size = args
+			this[DATA][memory] += [0 for i in range(size)]
+
+		elif I == I_TRANSFERCAP:
+			# TODO can only transfer 1 cap per call
+			target, cap = args
+
+			if has_cap(target):
+				target_caps = world[target][CAPS]
+				transfer_cap(cap, target_caps)
+
+		elif I == I_RECURSE:
+			target, gas, mem = args
+
+			if has_cap(target):
+
+				target_header = world[target][HEADER]
+				target_header[H_GAS] = min(header[H_GAS], gas)
+				target_header[H_MEM] = min(header[H_MEM], mem)
+
+				header[H_REC] = target
+				active = target
+				chain.append(target)
+
+		elif I == I_MEMSIZE:
+			target_index = args
+			this[DATA][target_index] = len(this[DATA])#must be serialized!
+
+		elif I == I_MEMWRITE:
+			memory, address, data = args
+			this[DATA][memory][address] = data
+
+		elif I == I_MEMCREATE:
+			this[DATA].append([])
+
+#H_STATUS, H_REC, H_GAS, H_MEM, H_IP
 code = [
 [I_MEMCREATE],
 [I_ALLOC, 0, 10],
@@ -110,172 +266,26 @@ code = [
 [I_MEMWRITE, 0, 3, 100],
 [I_MEMWRITE, 0, 4, 100],
 [I_MEMWRITE, 0, 5, 0],
-[I_MEMWRITE, 0, 6, 0],
-[I_MEMWRITE, 0, 7, 0],
+[I_MEMWRITE, 0, 6, 1],
+[I_MEMWRITE, 0, 7, 1],
+[I_MEMWRITE, 0, 8, 0],
+[I_MEMWRITE, 0, 9, 0],
 [I_CREATE, 0],
 [I_TRANSFERCAP, 1, 0],
 [I_RECURSE, 1, 100, 100],
+[I_ALLOC, 0, 10],
 ]
 
-#TODO: make CAPS dict? other data structure?
-process = [[S_NORMAL,0,1000,1000,0], {0}, code, []]
-world = [process]
+import json
+import os
+import sys
 
-def debug():
-	print("\nSTEP#"	 + str(STEP), INAMES[I])
-	for proc in world:
-		print(pretty(proc))
+from time import time
 
-start = 0
-chain = [start]
-
-while True:
-	current = chain[-1]
-	rec = world[current][HEADER][H_REC]
-	if rec != current:
-		chain.append(rec)
-	else:
-		break
-
-print("CHAIN", chain)
-#traverse H_REC here?
-
-STEP = 0
-while True:
-	STEP += 1
-
-	# TODO add jump-back logic to last process in parent chain that has sufficient resources
-
-	if len(chain) == 0:
-		print("END")
-		break
-	this = world[chain[-1]]
-
-	#TODO: only deserialize here, on demand
-
-	header = this[HEADER]
-	caps = this[CAPS]
-	code = this[CODE]
-
-	JB = False
-
-	def jump_back(condition, to=None):
-		global chain, JB
-		JB = True
-		header[H_STATUS] = condition
-		# TODO make this nicer
-		if to is None:
-			#world[active] = flat(world[active])
-			chain = chain[:-1]
-		else:
-			for i in range(len(world)-1, to, -1):
-				world[i] = flat(world[i])
-				chain = chain[:-1]
-
-	def set_cap(target):
-		caps.add(target)
-
-	def has_cap(target):
-		return target in caps
-
-	def transfer_cap(index, table):
-		if index in caps:
-			table.add(index)
-
-	ip = header[H_IP]
-	if ip >= len(code):#could also wrap around for shits and giggles
-		jump_back(S_OOC)
-		continue
-
-	print(ip, len(code))
-	C = code[ip]
-	I = C[0]
-	#TODO check length
-	args = C[1:]
-
-	gascost = IGASCOSTS[I]
-	if gascost is None:
-		gascost = args[1]#size arg for ALLOC
-
-	memcost = IMEMCOSTS[I]
-	if memcost is None:
-		if I == I_ALLOC:
-			memcost = args[1]#size arg for ALLOC
-		elif I == I_TRANSFERCAP:
-			memcost = sum([1 for c in args[1:] if c is not None])
-
-	for node_index, node in enumerate(chain):
-		node_header = world[node][HEADER]
-		if node_header[H_GAS] < gascost:
-			jump_back(S_OOG, node_index-1)
-			break
-		if node_header[H_MEM] < memcost:
-			jump_back(S_OOM, node_index-1)
-			break
-
-	if JB:
-		continue
-
-	# First check all conditions, then change the state!
-	for node_index, node in enumerate(chain):
-		world[node][HEADER][H_GAS] -= gascost
-		world[node][HEADER][H_MEM] -= memcost
-
-	debug()
-
-	header[H_IP] += 1
-
-	if I == I_CREATE:
-		memory = args[0]
-		index = len(world)
-
-		newproc = sharp(this[DATA][memory])
-		#print("NEW", newproc)
-		world.append(newproc)
-
-		set_cap(index)
-
-	elif I == I_ALLOC:
-		memory, size = args
-		this[DATA][memory] += [0 for i in range(size)]
-
-	elif I == I_TRANSFERCAP:
-		# TODO can only transfer 1 cap per call
-		target, cap = args
-
-		if has_cap(target):
-			target_caps = world[target][CAPS]
-			transfer_cap(cap, target_caps)
-
-	elif I == I_RECURSE:
-		target, gas, mem = args
-
-		if has_cap(target):
-
-			target_header = world[target][HEADER]
-			target_header[H_GAS] = min(header[H_GAS], gas)
-			target_header[H_MEM] = min(header[H_MEM], mem)
-
-			header[H_REC] = target
-			active = target
-			chain.append(target)
-
-	elif I == I_MEMSIZE:
-		target_index = args
-		this[DATA][target_index] = len(this[DATA])#must be serialized!
-
-	elif I == I_MEMWRITE:
-		memory, address, data = args
-		this[DATA][memory][address] = data
-
-	elif I == I_MEMCREATE:
-		this[DATA].append([])
-
-debug()
 
 from graphviz import Digraph
 
-def visualize():
+def visualize(world):
 
 	dot = Digraph()
 	dot.format = "svg"
@@ -285,10 +295,29 @@ def visualize():
 
 	#TODO highlight active
 
+	edges = 0
 	for pi, proc in enumerate(world):
 		for cap in proc[CAPS]:
+			edges += 1
 			dot.edge(name(pi), name(cap), color="black")
 
-	dot.render("graph", view=True)
+	if edges > 10:
+		dot.render("graphs/"+str(int(time()*10000)))#, view=True)
 
-#visualize()
+
+
+def main():
+	data = sys.stdin.read()
+	try:
+		code = json.loads(data)
+	except (UnicodeError, json.decoder.JSONDecodeError):
+		os._exit(0)
+
+	world = run(code)
+	visualize(world)
+	os._exit(0)
+
+if __name__ == "__main__":
+	import afl
+	afl.start()
+	main()
---
+++ runafl.sh
@@ -0,0 +1,1 @@
+py-afl-fuzz -m 400 -i inputs/ -o outputs/ -- python main.py
